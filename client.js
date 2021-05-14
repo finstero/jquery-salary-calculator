335525348126c9c9f09ba4e21e9f80e9b204774d
Olivia Finster <olivia.finster@gmail.com>
2021-05-14 16:24:41 -0500

    base function mostly working with no styling

--- client.js
+++ client.js
@@ -2,12 +2,60 @@ console.log('hello jS!')
 
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('hello jQ!');
     $('#submitButton').on('click', handleSubmit);
+    $('#tableBody').on('click', '.deleteButton', handleDelete);
 }
 
-function handleSubmit(){
-    let firstName = $('#firstNameIn').val();
-    
+let employees = [];
+
+function handleSubmit() {
+    //collect all input values and put them into person object
+    const person = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        id: $('#idIn').val(),
+        title: $('#titleIn').val(),
+        salary: $('#salaryIn').val(),
+    }
+    //add single employee/person to array of all employees
+    employees.push(person);
+    calculateCost();
+
+    $('#tableBody').append(`
+        <tr class="personInput">
+            <td scope="row">${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.id}</td>
+            <td>${person.title}</td>
+            <td>$${person.salary}</td>
+            <td>
+                <button class="deleteButton">Delete</button>
+            </td>
+        </tr>`)
+
+    //clear inputs
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idIn').val('');
+    $('#titleIn').val('');
+    $('#salaryIn').val('');
+}
+
+function calculateCost(){
+    let yearlyCost = 0;
+    for (person of employees){
+        yearlyCost += Number(person.salary);
+    }
+    let monthlyCost = yearlyCost/12;
+    $('#monthlyCost').text(`$${monthlyCost}`);
+    if (monthlyCost >= 20000){
+        $('#toChange').addClass('redBackground');
+    }
+}
+
+function handleDelete(){
+    $(this).closest('.personInput').remove();
+
 }
